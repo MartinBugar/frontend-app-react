093a208e737d4e9e0aa8b4319af47f69ed5c75d9
Martin <martin1.bugar@gmail.com>
2024-06-22 09:05:24 +0200

    init commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [messages, setMessages] = useState();
+
+    useEffect(() => {
+        axios.get('https://coral-app-n5qdo.ondigitalocean.app/1')
+            .then(response => {
+                setMessages(response.data.body);
+            })
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                message from the backend from DB : {messages}
+            </header>
+        </div>
+    );
 }
 
 export default App;
